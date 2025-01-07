c2220ad1f46466c28cf3f75dbff181d34b22ade6
Thiago Azevedo <thiagoazevedomh@gmail.com>
2025-01-07 11:18:52 -0300

    Desafio 1 realizado

---
+++ desafios/d001/resposta.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main>
+        <h1>Resultado Final</h1>
+        <?php
+            $num = $_GET['num'] ?? null;
+
+            if (!$num == null) {
+                $antecessor = $num - 1;
+                $sucessor = $num + 1;
+
+                echo "<p>O número escolhido foi <strong>". $num ."</strong> </p>";
+                echo "<p>O seu antecessor foi " . $antecessor . "</p>";
+                echo "<p>O seu sucessor foi " . $sucessor . "</p>";
+            } else {
+                echo "<p>Valor não informado</p>";
+            }
+        ?>
+        <p>
+            <a href="javascript:history.go(-1)"><button>Voltar</button></a>
+        </p>
+    </main>
+</body>
+
+</html>
