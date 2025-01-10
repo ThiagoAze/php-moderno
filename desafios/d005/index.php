358649302425ae9056af5e5e617a3d7528d26069
Thiago Azevedo <thiagoazevedomh@gmail.com>
2025-01-10 12:13:35 -0300

    Desafio 5 realizado

---
+++ desafios/d005/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <h1>Analisador de Número Real</h1>
+        
+        <form action="numero.php" method="get">
+            <label for="inum">Número Real:</label>
+            <input type="number" step="0.001" name="num" id="inum">
+
+            <input type="submit" value="Analisar">
+        </form>
+    </section>
+</body>
+</html>
---
+++ desafios/d005/numero.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section>
+        <h1>Analisador de Número Real</h1>
+
+        <?php
+            $num = $_GET['num'] ?? null;
+            $int = floor($num); //parte inteira
+            $frag = $num - $int; //parte fracionária
+
+            echo "Analisando o número <strong>" . number_format($num, 3, ',', '.') . "</strong> informado pelo usuário:";
+        ?>
+
+        <ul>
+            <?php 
+                echo '<li>A parte inteira do número é <strong>'. number_format($int, '0', ',', '.') .'</strong></li>';
+                echo '<li>A parte fracionária do número é <strong>'.number_format($frag, '3', ',', '.').'</strong></li>';
+            ?>
+        </ul>
+
+
+
+
+
+        <p>
+            <a href="javascript:history.go(-1)"><button>Voltar</button></a>
+        </p>
+    </section>
+</body>
+
+</html>
