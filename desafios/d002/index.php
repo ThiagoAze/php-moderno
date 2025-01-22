27ba319c90307138096838e0053dbcfa20cd06eb
Thiago Azevedo <thiagoazevedomh@gmail.com>
2025-01-22 10:41:40 -0300

    Desafio 2 refatorado

--- desafios/d002/index.php
+++ desafios/d002/index.php
@@ -11,18 +11,14 @@
         <h1>Trabalhando com números aleatórios</h1>
         <p>Gerando um número aleatório entre 0 e 100...</p>
         
-        <form method="post">
             <?php 
-                // Verifica se o botão foi pressionado
-                if(isset($_POST['gerar'])) {
-                    // Gera um número aleatório entre 1 e 100
-                    $num = rand(0, 100);
-                    echo "<p>O valor gerado foi <strong>". $num ."</strong></p>";
-                }
+                $num = mt_rand(0, 100); //Utiliza o mt_rand ao invés do rand, por ser mais rápido, moderno e seguro
+                // Também existe o random_int que criptografa os números aleatórios de forma segura, porém é lenta
+                
+                echo "<p>O valor gerado foi <strong>". $num ."</strong></p>";
             ?>
             
-            <input type="submit" name="gerar" value="Gerar número">
-        </form>
+            <button name="btn" onclick="javascript:document.location.reload()">&#x1F504 Gerar número</button>
     </section>
 </body>
 </html>
