40f8cac089a6820da2d5cc53bff6ead9861a36ce
Thiago Azevedo <thiagoazevedomh@gmail.com>
2025-02-14 11:11:41 -0300

    Desafio 6 realizado

---
+++ desafios/d006/divisor.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+    // Pegando valores dos inputs
+        $dividendo = $_GET['dividendo'] ?? 0;
+        $divisor = $_GET['divisor'] ?? 1;
+        $resto = 0;
+        
+        $resultado = floor($dividendo / $divisor);
+        var_dump($resultado);
+        
+        // Calculando resultados se existir valores
+        if($dividendo != 0 && $divisor != 1){
+            $resto = $dividendo % $divisor;
+        }
+    ?>
+    <main>
+        <h1>Anatomia de uma Divisão</h1>
+        <form action="<?= $_SERVER['PHP_SELF']?>" method="get">
+            <label for="idividendo">Dividendo</label>
+            <input type="number" name="dividendo" id="idividendo" value="<?=$dividendo?>">
+            
+            <label for="idivisor">Divisor</label>
+            <input type="number" name="divisor" id="idivisor" value="<?=$divisor?>">
+            <input type="submit" value="Analisar">
+        </form>
+        
+    </main>
+
+    <section>
+        <h2>Estrutura de Divisão</h2>
+        <table class="divisao">
+            <?php 
+                echo "<tr><td>$dividendo</td> <td>$divisor</td></tr>";
+                echo "<tr><td>$resto</td> <td>$resultado</td></tr>"
+            ?>
+        </table>
+    </section>
+
+</body>
+</html>
