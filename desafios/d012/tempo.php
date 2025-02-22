0df54f325e7025e1f364d279f7a19559232e1187
Thiago Azevedo <thiagoazevedomh@gmail.com>
2025-02-22 12:48:49 -0300

    Desafio 12 refatorado

--- desafios/d012/tempo.php
+++ desafios/d012/tempo.php
@@ -11,32 +11,31 @@
         $tempo = $_GET['tempo'] ?? 0;
         
         // Calculando o total de semanas, dias, horas, minutos e segundos
-        
         $semanas = floor($tempo / 604800); // 7 * 24 * 60 * 60
         $tempoRestante = $tempo % 604800;
 
         $dias = floor($tempoRestante / 86400); //24 * 60 * 60
         $tempoRestante %= 86400;
 
-        $horas = floor($tempoRestante / (3600)); //60 * 60
+        $horas = floor($tempoRestante / 3600); //60 * 60
         $tempoRestante %= (3600);
 
         $minutos = floor($tempoRestante / 60);
         $segundos = $tempoRestante % 60;
     ?>
     <main>
-        <h1></h1>
+        <h1>Calculadora de Tempo</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="tempo">Qual é o total de segundos?</label>
-            <input type="number" name="tempo" id="tempo">
+            <input type="number" name="tempo" id="tempo" min="0" required>
             <input type="submit" value="Calcular">
         </form>
     </main>
 
     <section>
-        <h2>totalizando Tudo</h2>
+        <h2>Totalizando Tudo</h2>
         <?php 
-            echo "Analisando o valor que você digitou, <strong>".number_format($tempo,'0','.','.')." segundos</strong> equivalem a um total de:";
+            echo "Analisando o valor que você digitou, <strong>".number_format($tempo,'0',',','.')." segundos</strong> equivalem a um total de:";
             echo "<ul>";
             echo " <li>$semanas semanas</li>";
             echo " <li>$dias dias</li>";
